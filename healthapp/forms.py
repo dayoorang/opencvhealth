df3c3749f7ec4a87458207eb7a5ec322c8dcea89
yangsunkang <dayoorang@gmail.com>
2021-10-06 16:22:54 +0900

    outline 수정

--- healthapp/forms.py
+++ healthapp/forms.py
@@ -6,3 +6,12 @@ class HealthForm(forms.ModelForm):
     class Meta:
         model = Health
         fields = ['repeats','set']
+
+    def __init__(self, *args, **kwargs):
+        super(HealthForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
+        self.fields['repeats'].label = 'Reps'
+        self.fields['set'].label = 'Sets'
